0bce1f6227bdf3d3b3ecf8365bcf4c41a6c9324e
Mohamed <nrookman_27@yahoo.com>
2022-10-20 16:03:16 +0200

    Link to user edit page get user data un user edit page

--- admin/users-edit.php
+++ admin/users-edit.php
@@ -1,20 +1,41 @@
 <?php
 require_once 'header.php';
+
+try {
+    $id = (int) $_GET['id'];
+
+    $sql = "SELECT * FROM users WHERE id = '$id'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $user = $stmt->fetch();
+
+    if (!$user) {
+        header("location: users.php");
+        die;
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+
 ?>
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Users</h1>
-        <a href="users.php" class="btn btn-warning"> < back</a>
+        <a href="users.php" class="btn btn-warning">
+            < back</a>
     </div>
     <div class="container card my-3 p-3">
         <form method="POST">
             <div class="form-floating my-3">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Name">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Name" value="<?= $user['name'] ?>">
                 <label for="name">Name</label>
             </div>
             <div class="form-floating my-3">
-                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" value="<?= $user['email'] ?>">
                 <label for="email">Email address</label>
             </div>
             <div class="form-floating my-3">
